e36e3cabd4012473c32cccaa8e669c68920361e9
Aldan <aldanrafa0108@gmail.com>
2025-01-24 21:32:16 +0700

    Tugas Commit

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'To-Do List',
+      title: 'List Pekerjaan',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
